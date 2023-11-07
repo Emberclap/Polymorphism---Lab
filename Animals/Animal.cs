a1384d07b6e0b5665b7e43b724d39cce07fea2cd
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-11-07 19:08:46 +0200

    Add project files.

---
+++ Animals/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favoriteFood;
+
+        protected Animal(string name, string favoriteFood)
+        {
+            this.Name = name;
+            this.FavoriteFood = favoriteFood;
+        }
+        public string Name { get; protected set; }
+        public string FavoriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.name} and my favourite food is {this.FavoriteFood}";
+        }
+    }
+}
---
+++ Shapes/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(5,6);
+
+            Console.WriteLine(circle.Draw());
+            Console.WriteLine(rectangle.Draw());
+        }
+    }
+}
